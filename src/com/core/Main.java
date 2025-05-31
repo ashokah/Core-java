2606cc9c0b6e7873d61ebdc369ec92a3febd1236
PhaniSri26 <phanisriyemineni1@gmail.com>
2025-05-31 20:54:40 +0530

    first commit

--- src/com/core/Main.java
+++ src/com/core/Main.java
@@ -3,8 +3,8 @@ package com.core;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("hello world");
+        int x=10;
+        System.out.println(x>>2);
 
     }
 }
