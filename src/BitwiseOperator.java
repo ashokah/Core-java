a4580d6cb0acc6ad655f7f079d42c10fad8c8f2a
marri hemalatha <marrihemalatha2019@gmail.com>
2025-06-01 19:26:32 +0530

    My first commit

--- src/BitwiseOperator.java
+++ src/BitwiseOperator.java
@@ -1,7 +1,31 @@
 public class BitwiseOperator {
     public static void main(String[] args) {
-        int a=10;
-        System.out.println(a++);
+        int a = 15;
+        int b = 8;
+        //Bitwise And
+        System.out.println(a & b);
 
+        //Bitwise Or
+        System.out.println(a | b);
+
+        //Bitwise Xor
+        System.out.println(a ^ b);
+
+        //right shift
+        System.out.println(a >> 1);
+        System.out.println(b >> 1);
+
+        System.out.println(a >> 3);
+        System.out.println(b >> 2);
+        System.out.println("the right filling zero");
+        System.out.println(a >>> 2);
+
+        // right shift filling zero
+        int x = 10;
+        System.out.println(x >>> 2);
+
+        //left shift
+        System.out.println(a << 1);
+        System.out.println(b << 1);
     }
 }
