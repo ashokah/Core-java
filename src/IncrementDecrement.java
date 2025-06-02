b6bdc9125349de2b9bd932c26b96e6bea2d298a0
marri hemalatha <marrihemalatha2019@gmail.com>
2025-06-02 21:09:11 +0530

    My first commit

---
+++ src/IncrementDecrement.java
@@ -0,0 +1,21 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a);
+
+        System.out.println(++b);
+        System.out.println(b++);
+        System.out.println(b);
+
+        System.out.println(--a);
+        System.out.println(a--);
+        System.out.println(a);
+
+        System.out.println(b--);
+        System.out.println(--b);
+        System.out.println(b);
+    }
+}
