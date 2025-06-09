ef1970676ff1e20af38a6a3ea154a5818a54fdda
marri hemalatha <marrihemalatha2019@gmail.com>
2025-06-09 19:46:51 +0530

    fourth commit patternNumber

--- src/PatternNumbers.java
+++ src/PatternNumbers.java
@@ -1,7 +1,7 @@
 public class PatternNumbers {
     public static void main(String[] args)
     {
-        int n=5;
+        int n=6;
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=i;j++)
