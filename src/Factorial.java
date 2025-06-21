6d8a77ce74b96351c814a11c5154e85a5f7b33f7
marri hemalatha <marrihemalatha2019@gmail.com>
2025-06-21 17:00:06 +0530

    sixth commit

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,10 +1,34 @@
 public class Factorial {
+    static long noRecursion(int num) {
+        long fact = 1;
+        while (num > 0)
+            fact = fact * num--;
+        return fact;
+    }
     public static void main(String[] args)
     {
-        int n=7 ,result=1;
-        for(int i=1;i<=n;i++) {
-            result = result * i;
-        }
-        System.out.println(result);
+        System.out.println("factorial of 5");
+        System.out.println(Factorial.noRecursion(5));
     }
 }
+
+
+//        int n=7 ,result=1;
+//        for(int i=1;i<=n;i++) {
+//            result = result * i;
+//        }
+//        System.out.println(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
