97ce4752bb8732c362322142f79b3802c2835d16
marri hemalatha <marrihemalatha2019@gmail.com>
2025-06-21 17:32:20 +0530

    Seventh commit

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -1,14 +1,14 @@
 public class PrimeNumber {
     public static void main(String[] args)
     {
-        int n=10;
+        int n=3;
         int count =0;
-            for(int i=1;i<=n;i++)
+            for(int i=2;i<=n;i++)
             {
                 if(n%i==0)
                     count++;
             }
-            if(count==2)
+            if(count==1)
             {
                 System.out.println("Prime Number");
             }
