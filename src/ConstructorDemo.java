3ecffff08b83f2a4b0c978eb67cfbd3bd9fb5a14
marri hemalatha <marrihemalatha2019@gmail.com>
2025-06-22 19:51:11 +0530

    Seventh commit

--- src/ConstructorDemo.java
+++ src/ConstructorDemo.java
@@ -1,7 +1,7 @@
 class Student {
-    int age;
+    /*int age;
     String name;
-
+ // constructor with no parameters
     public Student()
     {
         System.out.println("Constructor called:");
@@ -14,9 +14,25 @@ class Student {
     {
         Student s1 = new Student();
         System.out.println(s1.age);
-        System.out.println(s1.name);
+        System.out.println(s1.name);*/
 
 
 
+
+    int marks;
+    String name;
+
+    private Student(int m,String n)
+    {
+        marks = m;
+        name = n;
+        System.out.println(marks+ " " + name);
+    }
+    public static void main(String[] args)
+    {
+        Student s1 = new Student(10,"Hema");
+        Student s2= new Student(20,"Phani");
+        Student s3= new Student(30,"mohit Sir");
+        Student s4= new Student(40,"Ashok Sir");
     }
 }
