80cf1fb70287bc3584b2d7bf4746650e3104c3ca
marri hemalatha <marrihemalatha2019@gmail.com>
2025-06-22 20:00:56 +0530

    commit

--- src/ConstructorDemo.java
+++ src/ConstructorDemo.java
@@ -19,9 +19,9 @@ class Student {
 
 
 
-    int marks;
+    /*int marks;
     String name;
-
+      // parameterized constructor
     private Student(int m,String n)
     {
         marks = m;
@@ -33,6 +33,40 @@ class Student {
         Student s1 = new Student(10,"Hema");
         Student s2= new Student(20,"Phani");
         Student s3= new Student(30,"mohit Sir");
-        Student s4= new Student(40,"Ashok Sir");
+        Student s4= new Student(40,"Ashok Sir");*/
+
+
+    int marks;
+    String name;
+    int rollNo;
+
+    Student()
+    {
+        System.out.println("default constructor");
     }
+    // parameterized constrcutor
+    Student(int r)
+    {
+        rollNo =r;
+        System.out.println("parameterzied constructor:");
+        System.out.println(rollNo);
+    }
+
+    // constructor overloading
+    Student(String n, int m)
+    {
+        name = n;
+        marks =m;
+        System.out.println("constructor overloading");
+        System.out.println(name+ " "+ marks);
+    }
+    public static void main(String[] args)
+    {
+        Student s1= new Student();
+        Student s2 = new Student(25);
+        Student s3 = new Student("Hema",10);
+    }
+
+
+
 }
