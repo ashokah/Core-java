0471d13e4a8e6f43058a705f22a44d855c15d355
marri hemalatha <marrihemalatha2019@gmail.com>
2025-07-07 10:37:58 +0530

    Array3 Commit

---
+++ src/MaxArr.java
@@ -0,0 +1,13 @@
+public class MaxArr {
+    public static void main(String[] args) {
+        int arr[] = {2, 3, -1, 2, 55, 10};
+        int max=0;
+        for (int i = 0; i <= arr.length - 1; i++) {
+             if(max<=arr[i])
+             {
+                 max = arr[i];
+             }
+        }
+        System.out.println(max);
+    }
+}
